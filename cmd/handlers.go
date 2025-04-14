dd92243d0d5f30fd340aa46cc06a82c70e8b8f56
plux <zhanibek.gabitov@gmail.com>
2025-04-14 14:17:31 +0500

    handlers finished

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/jackc/pgconn"
 )
 
 type CreateUserForm struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type UpdateUserForm struct {
+	ID       int    `json:"id"`
+	NewName  string `json:"name"`
+	NewEmail string `json:"email"`
+}
+
+type GetUserForm struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (u *userInfo) CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -35,4 +55,99 @@ func (u *userInfo) CreateUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"message": "User created successfully",
 	})
+}
+
+func (u *userInfo) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	defer r.Body.Close()
+	var updateForm UpdateUserForm
+
+	if err := json.NewDecoder(r.Body).Decode(&updateForm); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+	if updateForm.ID == 0 {
+		http.Error(w, "User ID is required", http.StatusBadRequest)
+		return
+	}
+	if updateForm.NewName == "" && updateForm.NewEmail == "" {
+		http.Error(w, "Nothing to update", http.StatusBadRequest)
+		return
+	}
+	if err := u.userModel.UpdateUser(updateForm.ID, updateForm.NewName, updateForm.NewEmail); err != nil {
+		if err.Error() == "User not found" {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			http.Error(w, "Email alredy in use", http.StatusConflict)
+			return
+		}
+		http.Error(w, "Failed to update user", http.StatusInternalServerError)
+		return
+	}
+
+	updatedUser, err := u.userModel.GetUser(updateForm.ID)
+	if err != nil {
+		http.Error(w, "Failed to fetch updated user data", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "User updated successfully",
+		"user": map[string]interface{}{
+			"id":         updatedUser.ID,
+			"name":       updatedUser.Name,
+			"email":      updatedUser.Email,
+			"updated_at": updatedUser.UpdatedAt,
+		},
+	})
+}
+
+func (u *userInfo) GetUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	defer r.Body.Close()
+
+	idStr := r.URL.Query().Get("id")
+	if idStr == "" {
+		http.Error(w, "ID is required", http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID format", http.StatusBadRequest)
+		return
+	}
+	user, err := u.userModel.GetUser(id)
+	if err != nil {
+		if err.Error() == "User not found" {
+			http.Error(w, "User not found", http.StatusNotFound)
+
+		} else {
+			http.Error(w, "Faield to get user", http.StatusInternalServerError)
+
+		}
+		return
+	}
+	userInfo := map[string]interface{}{
+		"id":         user.ID,
+		"name":       user.Name,
+		"email":      user.Email,
+		"created_at": user.CreatedAt,
+		"updated_at": user.UpdatedAt,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(userInfo); err != nil {
+		log.Printf("Failed to encode response: %v", err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module gravitum_rest_api
 go 1.20
 
 require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
